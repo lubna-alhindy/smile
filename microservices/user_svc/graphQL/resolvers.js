d24430183749ef165d21580d14ff6ad5ccbf40b0
Ismail Alrefai <ismaeilsamba@gmail.com>
2022-06-20 23:53:38 +0300

    merge commit

--- microservices/user_svc/graphQL/resolvers.js
+++ microservices/user_svc/graphQL/resolvers.js
@@ -7,37 +7,34 @@ const resolvers = {
     Query: {
         getUser:  (root, args, { models }) => Controller.User.getUser(args ,models),
         getAllUser: (root, args, { models }) => Controller.User.getAllUser(models),
-        getAllPosts:(root, args, { models }) => Controller.Post.getAllPosts(models),
         getAllAds:(root, args, { models }) => Controller.Ads.getAllAds(models),
-
-        //to filter posts//
-        getAllGeneralPost: (root, args, { models }) => Controller.Post.getAllGeneralPost(models),
-        getAllPostOfUser: (root, args, { models }) => Controller.Post.getAllPostOfUser(args ,models),
-        getAllPostOfSubject: (root, args, { models }) => Controller.Post.getAllPostOfSubject(args ,models),
-        // end filter//
-
         getBansUser: (root, args, { models }) => Controller.User.getBansUser(models),
-        getAllComplaints:(root, args, { models }) => Controller.Complaints.getAllComplaints(models),
         getAllPostRequests:(root, args, { models }) => Controller.Post.getAllPostRequests(models),
+        getPost:(root, args, { models }) => Controller.Post.getPost(args,models),
+        getAllComplaints:(root, args, { models }) => Controller.Complaints.getAllComplaints(models),
+        getPosts:(root, args, { models }) => Controller.Post.getPosts(args,models),
+
+
+        getAllPostOfSubject: (root, args, { models }) => Controller.Post.getAllPostOfSubject(args ,models),
     },
 
     Mutation: {
-        createUser:(root, args, { models }) => Controller.User.createUser(args ,models),
         addPost:(root, args, { models }) => Controller.Post.addPost(args ,models),
-        like:(root, args , { models }) => Controller.Post.like(args ,models),
+        deletePost:(root, args, { models }) => Controller.Post.deletePost(args ,models),
+        approvalPostRequest:(root, args, { models }) => Controller.Post.approvalPostRequest(args ,models),
+        changeLike:(root, args , { models }) => Controller.Post.changeLike(args ,models),
         addComment:(root, args , { models }) => Controller.Post.addComment(args ,models),
         deleteComment:(root, args , { models }) => Controller.Post.deleteComment(args ,models),
-        addFavorite:(root, args , { models }) => Controller.Post.addFavorite(args ,models),
-        deleteFavorite:(root, args , { models }) => Controller.Post.deleteFavorite(args ,models),
-        banUser:(root, args, { models }) => Controller.User.banUser(args ,models),
-        unBanUser:(root, args, { models }) => Controller.User.unBanUser(args ,models),
+        changeFavorite:(root, args , { models }) => Controller.Post.changeFavorite(args ,models),
+        changeBanUser:(root, args, { models }) => Controller.User.changeBanUser(args ,models),
         addComplaint:(root, args, { models }) => Controller.Complaints.addComplaint(args ,models),
+        changeDoneComplaint:(root, args, { models }) => Controller.Complaints.changeDoneComplaint(args ,models),
         deleteComplaint:(root, args, { models }) => Controller.Complaints.deleteComplaint(args ,models),
-        makeComplaintAsDone:(root, args, { models }) => Controller.Complaints.makeComplaintAsDone(args ,models),
         addUsersUniversityNumbers:(root, args, { models }) => Controller.User.addUsersUniversityNumbers(args ,models),
         deleteUsersUniversityNumbers:(root, args, { models }) => Controller.User.deleteUsersUniversityNumbers(args ,models),
-        acceptPostRequest:(root, args, { models }) => Controller.Post.acceptPostRequest(args ,models),
-        deletePostRequest:(root, args, { models }) => Controller.Post.deletePostRequest(args ,models),
+        addAd:(root, args , { models }) => Controller.Ads.addAd(args ,models),
+        deleteAd:(root, args , { models }) => Controller.Ads.deleteAd(args ,models),
+
     }
 };
 
--- microservices/user_svc/graphQL/schema.js
+++ microservices/user_svc/graphQL/schema.js
@@ -20,9 +20,9 @@ const typeDefs = gql`
     gmail: String
     createdAt: Date
     updatedAt: Date
-    posts: [Posts]!
-    favorites: [Favorites]!
-    userUniversityNumbers: [UsersUniversityNumbers]!
+    posts: [Posts]
+    favorites: [Favorites]
+    userUniversityNumbers: [UsersUniversityNumbers]
   }
   
   type Posts {
@@ -31,14 +31,14 @@ const typeDefs = gql`
     type: String!
     title: String
     body: String!
-    UserId: Int!
+    userId: Int!
     createdAt: Date!
     updatedAt: Date!
-    likes: [Likes]!
-    likesCnt: Int!
+    likes: [Likes]
+    likesCnt: Int
     user: Users!
-    comments: [Comments]!
-    commentsCnt: Int!
+    comments: [Comments]
+    commentsCnt: Int
   }
   
   type PostRequests {
@@ -47,7 +47,7 @@ const typeDefs = gql`
     type: String!
     title: String
     body: String!
-    UserId: Int!
+    userId: Int!
     createdAt: Date!
     updatedAt: Date!
     user: Users!
@@ -55,29 +55,29 @@ const typeDefs = gql`
   
   type Likes {
     id: Int!
-    UserId: Int!
-    PostId: Int!
+    userId: Int!
+    postId: Int!
     user: Users!
   }
   
   type Comments {
     id: Int!
     body: String!
-    UserId: Int!
-    PostId: Int!
+    userId: Int!
+    postId: Int!
     user: Users!
   }
   
   type Favorites {
     id: Int!
-    UserId: Int!
-    PostId: Int!
+    userId: Int!
+    postId: Int!
     post: Posts!
   }
   
   type Bans {
     id: Int!
-    UserId: Int!
+    userId: Int!
     user: Users
   }
   
@@ -98,47 +98,46 @@ const typeDefs = gql`
   
   type UsersUniversityNumbers{
     id: Int!
-    UserId: Int!
+    userId: Int!
     universityNumber: Int!
     year: String!
     user: Users!
   }
   
   type Query {
-    getUser(id: Int!): Users
+    getUser(id: Int! ,favorite: Boolean ,universityNumber: Boolean ,posts: Boolean): Users
     getAllUser: [Users!]!
-    getAllPosts: [Posts]!
     getAllAds:[Ads]!
-    
-    getAllGeneralPost: [Posts]!
-    getAllPostOfUser(UserId: Int!): [Posts]!
-    getAllPostOfSubject(subjectId: [Int]!): [Posts]!
-    
     getBansUser: [Bans]!
-    getAllComplaints: [Complaints]!
     getAllPostRequests: [PostRequests]!
+    getPost(id: Int! ,like: Boolean ,comment: Boolean): Posts
+    getAllComplaints: [Complaints]!
+    getPosts(filter: String): [Posts]
+    
+    getAllPostOfSubject(subjectId: [Int]!): [Posts]! 
   }
   
   type Mutation {
-    createUser(roleName: String! ,firstName: String! ,lastName: String! ,email: String! ,password: String!): Users
-    addPost(subjectId: Int ,type: String! ,title: String ,body: String! ,UserId: Int!): Posts
-    like(UserId: Int! ,PostId: Int!): Void
-    addAd(title: String! ,body: String! ,expireIn: Date!): Ads
-    addComment(body: String! ,UserId: Int! ,PostId: Int!): Comments
+    addPost(subjectId: Int ,type: String! ,title: String ,body: String! ,userId: Int!): Posts
+    deletePost(id: Int!): Void
+    approvalPostRequest(id: Int! ,cheack: Boolean!): PostRequests
+    changeLike(userId: Int! ,postId: Int!): Void
+    addComment(body: String! ,userId: Int! ,postId: Int!): Comments
     deleteComment(id: Int!):Void
-    addFavorite(UserId: Int! ,PostId: Int!): Favorites
-    deleteFavorite(id: Int!): Void
-    banUser(UserId: Int!): Bans
-    unBanUser(UserId: Int!): Void
-    addComplaint(UserId: Int! ,title: String ,body: String!): Complaints
+    changeFavorite(userId: Int! ,postId: Int! ): Favorites
+    changeBanUser(userId: Int! ,choise: Boolean): Bans
+    addComplaint(userId: Int! ,title: String ,body: String!): Complaints
+    changeDoneComplaint(id: Int! ,choise: Boolean): Complaints
     deleteComplaint(id: Int!): Void
-    makeComplaintAsDone(id: Int!): Complaints
-    addUsersUniversityNumbers(UserId: Int!,universityNumber: Int! ,year: String!): UsersUniversityNumbers
+    addUsersUniversityNumbers(userId: Int!,universityNumber: Int! ,year: String!): UsersUniversityNumbers
     deleteUsersUniversityNumbers(id: Int!): Void
-    acceptPostRequest(id: Int!): PostRequests
-    deletePostRequest(id: Int!): Void
+    addAd(title: String! ,body: String! ,expireIn: Date!): Ads
+    deleteAd(id: Int!): Void
   }
 `;
 
+//signup,login,edit profile
 //add image to post and ads
+//cheack if ban and role befor any request
+
 module.exports = typeDefs;
