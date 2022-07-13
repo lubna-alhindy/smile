7eb3dfbde683a9e2815568e33e1c3871fdc7f229
Loubna Alhindy <loubnahn01@gmail.com>
2022-07-13 15:35:50 +0300

    add type

--- microservices/quize_svc/graphQl/schema.js
+++ microservices/quize_svc/graphQl/schema.js
@@ -4,6 +4,24 @@ const typeDefs = gql`
   scalar Date
   scalar Void
   
+  type quizs {
+    id: Int!
+    subjectName: String!
+    question: String!
+    answer: String!
+    createdAt: Date
+    updatedAt: Date
+  }
+  
+  typy quizRequests{
+    id: Int!
+    subjectName: String!
+    question: String!
+    answer: String!
+    createdAt: Date
+    updatedAt: Date
+  }
+  
   type Query {
    
   }
